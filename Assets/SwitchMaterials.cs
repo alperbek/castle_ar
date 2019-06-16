39a28a35cb0fc2922996a2035e815c874e3cecc3
Cagri Hakan Zaman <zaman@mit.edu>
2019-06-16 02:18:47 -0400

    First Commit

---
+++ Assets/SwitchMaterials.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMaterials : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject model;
+    public Material[] mats;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeMat (Material mat){
+
+        mats = model.GetComponent<Renderer>().materials;
+        mats[0] = mat;
+        model.GetComponent<Renderer>().materials = mats;
+        model.GetComponent<Renderer>().material = mat;
+        Debug.Log("Changed mat");
+    }
+}
